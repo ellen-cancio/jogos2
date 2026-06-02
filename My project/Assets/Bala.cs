b8ef50330ab68cf3befab69d90b62ea597fc7530
saralinhares <ellen.cruz.cancio@gmail.com>
2026-06-02 12:01:47 -0300

    a

--- My project/Assets/Bala.cs
+++ My project/Assets/Bala.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
-public class Bala : MonoBehaviour {
-    void OnCollisionEnter(Collision collision) {
-        Destroy(gameObject);
+
+public class Bala : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // O 0.05f faz a bala esperar uma fração de segundo antes de sumir.
+        // Assim dá tempo do alvo perceber que apanhou!
+        Destroy(gameObject, 0.05f); 
     }
 }
